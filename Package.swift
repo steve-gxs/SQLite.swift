4f32e0d231f1d4c0b13db285f0870f0f382cdeb7
steve-gxs <108255280+steve-gxs@users.noreply.github.com>
2023-01-03 18:14:56 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
     name: "SQLite.swift",
     platforms: [.iOS(.v8)],
-    products: [.library(name: "SQLite", targets: ["SQLiteBinary"])],
+    products: [.library(name: "SQLite", targets: ["SQLite"])],
     targets: [
         .binaryTarget(
-            name: "SQLiteBinary",
+            name: "SQLite",
             url: "https://github.com/steve-gxs/SQLite.swift/raw/main/SQLite.xcframework.zip",
             checksum: "64dd1fac0a9b040556d78703237cf322b28827c05e9931b02a0d7cf7a0d98f4d"
         )
